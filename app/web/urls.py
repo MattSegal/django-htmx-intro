a32d42a44d18333381a33ceff3cd37e339283e4f
Matthew Segal <mattdsegal@gmail.com>
2021-06-05 14:40:19 +1000

    Add htmx

--- app/web/urls.py
+++ app/web/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index_view, name="index"),
+    path("contact/", views.contact_view, name="contact"),
 ]
--- app/web/views.py
+++ app/web/views.py
@@ -5,16 +5,17 @@ from django.contrib import messages
 from .forms import ContactForm
 
 
-@require_http_methods(["GET", "POST"])
+@require_http_methods(["GET"])
 def index_view(request):
-    if request.method == "POST":
-        # It's a form submission.
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Contact request submitted")
-    else:
-        # It's a regular page visit.
-        form = ContactForm()
-
+    form = ContactForm()
     return render(request, "web/index.html", {"form": form})
+
+
+@require_http_methods(["POST"])
+def contact_view(request):
+    form = ContactForm(request.POST)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Contact request submitted")
+
+    return render(request, "web/htmx/contact_form.html", {"form": form})
